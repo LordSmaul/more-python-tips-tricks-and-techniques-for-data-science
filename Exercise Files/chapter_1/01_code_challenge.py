6bd46b96b303e91c20c7f7e64649786c227dc78a
shschmahl42 <Sam.Schmahl@gmail.com>
2025-08-27 11:50:56 -0500

    Ch 1 (all) updates

---
+++ Exercise Files/chapter_1/01_code_challenge.py
@@ -0,0 +1,18 @@
+# Python code​​​​​​‌‌‌​​​​‌​​‌​​‌​​​‌​‌‌‌‌​​ below
+import io
+import sys
+
+def get_object_info(obj):
+    # Capture the output of help() function
+    old_stdout = sys.stdout
+    sys.stdout = buffer = io.StringIO()
+    help(obj)
+    sys.stdout = old_stdout
+    
+    # Get the first line of the help output
+    doc_string = buffer.getvalue().split('\n')[0].strip()
+    
+    # Get the type of the object as a string
+    type_string =  type(obj)
+
+    return (doc_string, type_string)
